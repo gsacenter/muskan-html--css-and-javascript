734c947d9d4e9cf5a8a0a8533a7e683e92a8e9d8
gsacenter <gsacenter810@gmail.com>
2025-06-30 14:34:42 +0530

    Refactor Cart component: update item display and improve styling

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import {StoreContext} from '../../context/StoreContext'
-
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
-  const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+  const cartIsEmpty = Object.values(cartItems).every(quantity => quantity === 0);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
-          <p>Items</p>
+          <p>Image</p>
           <p>Title</p>
           <p>Price</p>
           <p>Quantity</p>
@@ -20,24 +20,32 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0)
-          {
-            return (
-              <div className='cart-items-title cart-items-item'>
-              <img src={item.image} alt="" />
-              <p>{item.name}</p>
-              <p>{item.price}</p>
-              <p>{cartItems[item._id]}</p>
-              <p>{item.price * cartItems[item._id]}</p>
-              <p>x</p>
-              </div>
-            )
-          }
-        })}
+        {!cartIsEmpty ? (
+          food_list.map((item, index) => {
+            if (cartItems[item._id] > 0) {
+              return (
+                <div>
+                  <div className="cart-items-title cart-items-item" key={item._id} >
+                    <img src={item.image} alt={item.name} />
+                    <p>{item.name}</p>
+                    <p>${item.price}</p>
+                    <p>{cartItems[item._id]}</p>
+                    <p>${item.price * cartItems[item._id]}</p>
+                    <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                  </div>
+                  <hr />
+                </div>
+
+              );
+            }
+            return null;
+          })
+        ) : (
+          <p className="cart-empty">Your cart is empty.</p>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
